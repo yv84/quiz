150fb3c33fccc738f797d04f8369d38f663ef726
Vladimir Yudintsev <yv87@ya.ru>
2014-11-13 19:20:53 +0500

    flask cli

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,13 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, static_url_path='')
-app.config.from_object('config')
+app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
 
 
 from app.routes import index
 
+@app.cli.command()
+def initdb():
+    """Initialize the database."""
+    print('Init the db')
--- run.py
+++ run.py
@@ -1,2 +1,3 @@
-from app import app
-app.run(debug = True)
+# python -m flask --app=app [command]
+# python -m flask --app=app --debug run
+# python -m flask --app=app initdb
