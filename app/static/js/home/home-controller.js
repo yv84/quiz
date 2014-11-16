d4e68495006df77e73dc056e42e120e28a6f3349
Vladimir Yudintsev <yv87@ya.ru>
2014-11-17 01:15:30 +0500

    colored answers

--- app/static/js/home/home-controller.js
+++ app/static/js/home/home-controller.js
@@ -10,12 +10,19 @@ angular.module('job_quiz')
     keyevents = function(key, callback){
       keyboardManager.bind(key, function() {
         console.log(key);
-        callback();
+        callback(key);
         }, {
   		  'type': 'keyup',
   	  });
     };
-    pass = function () {
+
+    get_answer = function (index) {
+      if ($($("#answers>p a").get([+index-1])).hasClass('answer_true')) {
+        $($("#answers>p a").get([+index-1])).addClass('answered_true')
+      }
+      else if ($($("#answers>p a").get([+index-1])).hasClass('answer_false')) {
+        $($("#answers>p a").get([+index-1])).addClass('answered_false')
+      };
     }
 
     route_back = function () {
@@ -31,42 +38,17 @@ angular.module('job_quiz')
     keys = {
       'q': route_back,
       'w': route_next,
-      '1': pass,
-      '2': pass,
-      '3': pass,
-      '4': pass,
-      '5': pass,
-      '6': pass,
-      '7': pass,
-      '8': pass,
-      '9': pass
+      '1': get_answer,
+      '2': get_answer,
+      '3': get_answer,
+      '4': get_answer,
+      '5': get_answer,
+      '6': get_answer,
+      '7': get_answer,
+      '8': get_answer,
+      '9': get_answer
     }
     for (key in keys){
       keyevents(key, keys[key]);
     };
-
-// function r_change1(myRadio) {
-//   if ($(myRadio).hasClass("answer_true")) {
-//     $(myRadio).addClass("btn-success")
-//   }
-//   else {
-//     $(myRadio).addClass("btn-danger")
-//   }
-//   return false
-// };
-// $('#answers').unbind();
-//
-// $('#answers').find('>a').on('click', function() {
-//   if ($(this).hasClass('answer_true')) {
-//     $(this).css("background", "#1AF029");
-//   }
-//   if ($(this).hasClass('answer_false')) {
-//     return $(this).css("background", "#F5111D");
-//   }
-// });
-//
-// $('html').bind('keypress', function(e) {
-
-// });
-
 }])
