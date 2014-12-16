f41eed66dee867f8041df57b87eee23e36ce2cd1
Vladimir Yudintsev <yv87@ya.ru>
2014-12-16 08:11:31 +0500

    answers hotkeys

--- app/static/js/home/home-controller.js
+++ app/static/js/home/home-controller.js
@@ -11,6 +11,20 @@ angular.module('job_quiz')
     $scope.next = "#/ask/" + (+$scope.askid+1);
     $scope.asks = HomeFactory;
 
+    $scope.number_keys = {};
+    $scope.setNumeralKeys = function (index, cb) {
+        if (index in [1,2,3,4,5,6,7,8,9]) {
+            $scope.number_keys[index + ""] = cb;
+        } else {console.error("error")};
+        hotkeys.bindTo($scope).add({
+            combo: index + "",
+            description: index + "",
+            callback: function() {
+                $scope.number_keys[index]();
+            }
+        })
+    };
+
     hotkeys.bindTo($scope)
       .add({
         combo: 'q',
@@ -25,7 +39,7 @@ angular.module('job_quiz')
         callback: function() {
           route_next();
         }
-      });
+      })
 
     route_back = function () {
       $scope.askid = +$scope.askid - 1;
@@ -76,28 +90,37 @@ angular.module('job_quiz')
       descr: "@",
       correctness: "@",
       index: "@",
+      numeralkeys: "&",
     },
     restrict: 'A',
     template: '<a href="#" class="btn btn-block answer_true">{{index}}: {{descr}}</a>',
     controller: function ($scope) {
+        $scope.activate = function (element) {
+            if ($scope.correctness === "true") {
+                element.addClass($scope.tr);
+            } else {
+                element.addClass($scope.f);
+            }
+        };
+        $scope.deactivate = function (element) {
+            if ($scope.correctness === "true") {
+                element.removeClass($scope.tr);
+            } else {
+                element.removeClass($scope.f);
+            }
+        }
     },
     link: function (scope, element, attrs) {
-      element.bind("mouseenter", function () {
-        if (scope.correctness === "true") {
-          element.addClass(scope.tr);
-        } else {
-          element.addClass(scope.f);
+        element.bind("click", function () {
+            scope.activate(element);
+            return false;
+        });
+        hotKeyCallback = function () {
+            scope.activate(element);
         }
-      }),
-      element.bind("mouseleave", function () {
-        if (scope.correctness === "true") {
-          element.removeClass(scope.tr);
-        } else {
-          element.removeClass(scope.f);
-        }
-      }),
-      element.bind("click", function () {
-      })
+        scope.numeralkeys({index: scope.index,
+            cb: hotKeyCallback
+        });
     }
   };
 })
