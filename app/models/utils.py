8a1edc76794080f74922c9d07d74c0a9f5792cfe
Vladimir Yudintsev <yv87@ya.ru>
2014-12-17 15:57:25 +0500

    convert_data for json obj

--- app/models/utils.py
+++ app/models/utils.py
@@ -1,2 +1,23 @@
+import re
+
 def convert_data(data):
-    return data
+    # split_asks -> split_stings -> get ask/ get answers -> push
+    arr = []
+    i = 1
+    _data = data[data.find("$")+1:]
+    for ask in _data.split("$"):
+        _ask = "".join(["$", ask])
+        obj = {"number": i, "answers": [], }
+        for line in _ask.split("\n"):
+            if line.startswith("$"): # in ("$", "#", "@"):
+                print(line)
+                obj["question"] = line[2:]
+            elif line.startswith("#"):
+                print(line)
+                obj["answers"].append({"answer": line[2:], "correct": False})
+            elif line.startswith("@"):
+                print(line)
+                obj["answers"].append({"answer": line[2:], "correct": True})
+        arr.append(obj)
+        i += 1
+    return arr
--- app/test/test_model.py
+++ app/test/test_model.py
@@ -1,29 +1,31 @@
 # /usr/bin/python3
 
 # python3 -m unittest -v app.test.test_model
+
 import unittest
 from app.models.utils import convert_data
 
-data_text = """==================================================================
+data_text = """
+==================================================================
 $ 1/1 - Абсд1
-# 1- Вопрос 11
-# 2- Вопрос 12
-# 3- Вопрос 13
-# 4- Вопрос 14
-@ 5- Вопрос 15
+# Вопрос 11
+# Вопрос 12
+# Вопрос 13
+# Вопрос 14
+@ Вопрос 15
 ===============================
 $ 1/2 - Абсд2
-# 1- Вопрос 11
-@ 2- Вопрос 12
-# 3- Вопрос 13
-# 4- Вопрос 14
-# 5- Вопрос 15
+# Вопрос 21
+@ Вопрос 22
+# Вопрос 23
+# Вопрос 24
+# Вопрос 25
 ===============================
 """
 data_to_json = [
     {
     "number": 1,
-    "question": "Абсд1",
+    "question": "1/1 - Абсд1",
     "answers": [
         {
           "answer": 'Вопрос 11',
@@ -48,26 +50,26 @@ data_to_json = [
     ]},
     {
     "number": 2,
-    "question": "Абсд2",
+    "question": "1/2 - Абсд2",
     "answers": [
         {
-          "answer": 'Вопрос 11',
+          "answer": 'Вопрос 21',
           "correct": False
         },
         {
-          "answer": 'Вопрос 12',
+          "answer": 'Вопрос 22',
           "correct": True
         },
         {
-          "answer": 'Вопрос 13',
+          "answer": 'Вопрос 23',
           "correct": False
         },
         {
-          "answer": 'Вопрос 14',
+          "answer": 'Вопрос 24',
           "correct": False
         },
         {
-          "answer": 'Вопрос 15',
+          "answer": 'Вопрос 25',
           "correct": False
         }
     ]},
@@ -84,6 +86,7 @@ class TestCase(unittest.TestCase):
         pass
 
     def testMessageClass(self):
+        self.maxDiff = None
         self.assertEqual(convert_data(data_text), data_to_json)
 
 
